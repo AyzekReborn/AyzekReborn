bcb9e30f2580d8293998d06f93bcb64f8570bc28
Лач <iam@lach.pw>
2019-12-19 01:34:00 +0500

    Files missing in last commit

--- src/bot/argument.ts
+++ src/bot/argument.ts
@@ -2,13 +2,13 @@ import { ArgumentType } from "../command/arguments";
 import { ParseEntryPoint } from "../command/command";
 import { UserDisplayableError } from "../command/error";
 import StringReader from "../command/reader";
-import { Api } from "../model/api";
+import { Api, isApi } from "../model/api";
 import { User } from "../model/conversation";
 
 export class UserArgumentType extends ArgumentType<User<any>> {
 	async parse<P>(ctx: ParseEntryPoint<P>, reader: StringReader): Promise<User<any>> {
-		if (!(ctx.sourceProvider instanceof Api)) throw new Error('Can\'t use user argument type on non-ayzek command dispatchers');
-		const user = await ctx.sourceProvider.apiLocalUserArgumentType.parse(ctx, reader);
+		if (!isApi(ctx.sourceProvider as any)) throw new Error("Can't use user argument type on non-ayzek command dispatchers");
+		const user = await (ctx.sourceProvider as unknown as Api<any>).apiLocalUserArgumentType.parse(ctx, reader);
 		await ctx.ayzek.attachToUser(user);
 		return user;
 	}
