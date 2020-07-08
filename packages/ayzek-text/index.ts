4e4cf8d8b040a94dfa8b160871b13d359da35ace
Лач <iam@lach.pw>
2020-07-08 10:20:20 +0000

    Text: Template literals api

--- packages/ayzek-text/index.ts
+++ packages/ayzek-text/index.ts
@@ -15,15 +15,17 @@ export function castOpaquePart(t: OpaqueTextPart, neededOpaqueType: symbol): any
 	return t.opaque as any;
 }
 
-export type FormattingTextPart = {
-	type: 'formatting',
+export type FormattingDesc = {
 	bold?: boolean,
 	underlined?: boolean,
 	color?: string,
 	preserveMultipleSpaces?: boolean,
 	italic?: boolean,
-	data: Text,
 }
+export type FormattingTextPart = {
+	type: 'formatting',
+	data: Text,
+} & FormattingDesc;
 export type CodeTextPart = {
 	type: 'code',
 	lang: string,
@@ -46,3 +48,14 @@ export type Text = TextPart;
 export function joinText(joiner: Text, arr: Text[]): Text[] {
 	return arr.flatMap((e: any, index: number) => index ? [joiner, e] : [e]);
 }
+
+export type Formatter = (strings: TemplateStringsArray, ...format: Text[]) => Text;
+export function wrappedText(wrapper: (input: Text) => Text): Formatter {
+	return (strings, ...format) => wrapper(strings.flatMap((e, index) => index ? [format[index - 1], e] : [e]));
+}
+export const text = wrappedText(t => t);
+export const formattedText = (f: FormattingDesc) => wrappedText(t => ({
+	type: 'formatting',
+	...f,
+	data: t,
+} as FormattingTextPart));
