0a2dc39b5af7472c8e522ef189e668ac84df7674
Yaroslav Bolyukin <iam@lach.pw>
2020-12-10 15:48:52 +0300

    feat(FloodPlugin): translations
    Signed-off-by: Yaroslav Bolyukin <iam@lach.pw>

--- packages/ayzek-plugin-flood/index.ts
+++ packages/ayzek-plugin-flood/index.ts
@@ -1,6 +1,6 @@
 import { intArgument } from '@ayzek/command-parser/arguments';
 import { File, Image } from '@ayzek/core/model/attachment';
-import { command, PluginCategory, PluginInfo } from '@ayzek/core/plugin';
+import { command, PluginBase, PluginCategory } from '@ayzek/core/plugin';
 import { emit } from '@meteor-it/xrest';
 import { whoCommand } from './who';
 
@@ -13,22 +13,22 @@ type CatApiResponse = {
 	url: string,
 }[];
 
-const catOBotCommand = command('cat-o-bot')
-	.thenArgument('Количество', intArgument(1, 20), b =>
-		b.executes(async ctx => {
+const catOBotCommand = ({ t }: Plugin) => command('cat-o-bot')
+	.thenArgument('Количество', intArgument(1, 20), b => b
+		.executes(async ctx => {
 			const catImages: CatApiResponse = (await emit('GET', 'https://api.thecatapi.com/v1/images/search', {
 				query: {
 					limit: ctx.getArgument('Количество'),
 				},
 			})).jsonBody;
 			if (!catImages || catImages.length === 0)
-				return await ctx.source.send('Коты не получены (');
+				return await ctx.source.send(t`Cats not found`);
 			const attachments = await Promise.all(catImages.map(e => e.url.endsWith('.gif') ? File.fromUrl('GET', e.url, {}, 'image.gif', 'image/gif') : Image.fromUrl('GET', e.url, {}, 'photo.jpeg', 'image/jpeg')));
-			await ctx.source.send('Лови котов', attachments);
+			await ctx.source.send(t`Cats sent!`, attachments);
 		}, 'Порт котобота жарвиса на айзека'),
 	);
 
-export default class implements PluginInfo {
+export default class Plugin extends PluginBase {
 	name = 'FloodPlugin';
 	author = 'НекийЛач';
 	description = 'Плагин для флуда';
